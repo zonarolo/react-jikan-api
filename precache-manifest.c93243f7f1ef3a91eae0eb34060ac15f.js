444c6ad51d3d380d01f4779753565c139e0466e9
Juan Rolo <juanluis211@outlook.com>
2021-05-23 23:41:02 +0200

    Updates

---
+++ precache-manifest.c93243f7f1ef3a91eae0eb34060ac15f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33612ccfac7477e2417dd02dba0b3c4",
+    "url": "/react-jikan-api/index.html"
+  },
+  {
+    "revision": "29a9107e4d7703e41c60",
+    "url": "/react-jikan-api/static/css/2.69b2cdbe.chunk.css"
+  },
+  {
+    "revision": "a9fd7a693cccc395b1e0",
+    "url": "/react-jikan-api/static/css/main.a1648652.chunk.css"
+  },
+  {
+    "revision": "29a9107e4d7703e41c60",
+    "url": "/react-jikan-api/static/js/2.121b48c9.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/react-jikan-api/static/js/2.121b48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9fd7a693cccc395b1e0",
+    "url": "/react-jikan-api/static/js/main.dc8fb17c.chunk.js"
+  },
+  {
+    "revision": "9af0b8e4fe7a6c2f57b8",
+    "url": "/react-jikan-api/static/js/runtime-main.a14cec5a.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/react-jikan-api/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/react-jikan-api/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/react-jikan-api/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "1f5f16de864451af07b4a671967f0553",
+    "url": "/react-jikan-api/static/media/primeicons.1f5f16de.ttf"
+  },
+  {
+    "revision": "35c93c4c5ecb5aed2f1e9d537f66aa7c",
+    "url": "/react-jikan-api/static/media/primeicons.35c93c4c.woff"
+  },
+  {
+    "revision": "c5349d1a1abc61ce03b3090ae0614ac7",
+    "url": "/react-jikan-api/static/media/primeicons.c5349d1a.eot"
+  },
+  {
+    "revision": "e285f4aaadf6e99379d52c0ab6bc446c",
+    "url": "/react-jikan-api/static/media/primeicons.e285f4aa.svg"
+  }
+]);
